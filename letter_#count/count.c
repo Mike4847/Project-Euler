660d48df42e7d803fc5b5bdc30fa9e242f2ba3d4
Mike4847 <michaelelemanl19@students.tukenya.ac.ke>
2024-11-05 17:41:27 +0300

    modified:   letter_#count/count.c deleted:    letter_#count/main.c

--- letter_#count/count.c
+++ letter_#count/count.c
@@ -77,3 +77,31 @@ int countWords(char *number)
 
   return count;
 }
+
+extern void numToWords(int A, char *number);
+extern int countWords(char *number);
+
+int main()
+{
+  // call the num to words
+  // declare a variable that can hld string
+  char *res = (char *)malloc(sizeof(char) * 200);
+  numToWords(145, res);
+
+  int cnt;
+  cnt = countWords(res);
+
+  // print cnt number
+  printf("%i", cnt);
+
+  // forloop
+
+  // for (int i=0; i<200 ; i++)
+  // {
+
+  // }
+  // free the memory
+  free(res);
+
+  return (0);
+}
