7f6aeb539a266b27d740c5f5f8640c5c49ffd137
Mike4847 <michaelelemanl19@students.tukenya.ac.ke>
2024-11-30 23:59:24 +0300

    Setting Up the Environment for the FactorialSum IN GO

---
+++ FactorialSum/Factorial.go
@@ -0,0 +1,19 @@
+package main 
+
+// import the io library
+import ("fmt")
+
+func main() {
+	fmt.Println("AM back baby!")
+}
+
+
+// func to get the sum of individual elements in an array 
+func Factorial(x int ){
+	//variable to hod the value of the 
+
+	for i := 2 ; i <= x; i++ {
+
+	}
+
+}
---
+++ FactorialSum/go.mod
@@ -0,0 +1,3 @@
+module FactorialSum
+
+go 1.23.2
