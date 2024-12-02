66512241addd2137ea79bd3a753bf0f38259d877
Mike4847 <michaelelemanl19@students.tukenya.ac.ke>
2024-12-02 18:39:03 +0300

    Amicable number setup

---
+++ Amicable Numbers/main.py
@@ -0,0 +1,53 @@
+
+#function for calculating amicable numbers 
+def Amicable(n:int)->int:
+  """ function takes the parameter(input) and
+    calculate the perfect divisor of the number.
+    The divisors should be less than n.
+    
+    
+    Return values is the number(n) and the sum of the elements(perfect divisors)"""
+  result = []
+  for i in range(1,n):
+    if n % i == 0:
+      result.append(i)
+  
+  return sum(result)
+
+def main():
+
+
+  #initialize a dictionary 
+  AmicableNumbers ={}
+  # loop from 1 to 1000 to populate the keyvalue pair.
+  for i in range(1,1000):
+    AmicableNumbers[i] = Amicable(i)
+    # print(f"{i} : {Amicable(i)}")
+
+
+  # list containing amicable numbers
+  result = set()
+  # check for amicable numbers in the dic
+  
+  
+
+    
+  
+  # #check if numbers are amicable
+  # for j in range(1, 1000):
+  #   b = Amicable(j)
+  #   if b in AmicableNumbers and AmicableNumbers[b] == j and j != b:
+  #       result.update([j, b])
+  for j in range(1, 1000):
+    b = Amicable(j)
+    print(f"j: {j}, b: {b}, AmicableNumbers[b]: {AmicableNumbers.get(b)}")
+    if b in AmicableNumbers and AmicableNumbers[b] == j and j != b:
+        result.update([j, b])
+  
+  print(result)
+  print("The sum of amicable numbers in range 1...1000 is: %d\n " % sum(result))
+
+
+# drivercode 
+if __name__ == "__main__":
+  main()
