545bd935ca2d69210d7c24717c81509bcaf569ef
Mike4847 <michaelelemanl19@students.tukenya.ac.ke>
2024-12-14 10:59:57 +0300

    Final answer

--- Non-Abundant Sum/main.py
+++ Non-Abundant Sum/main.py
@@ -40,24 +40,32 @@ def abundant():
   """
   return [i for i in range(1,ABUNDANT_NO_LIMIT) if is_abundant(i)]
 
-def main():
-  positive_integers = []
-  abundant_numbers = abundant()       # populates with abundant numbers
-  sum_of_abundant = []
+def non_abundant_sum(n:set)->int:
+  non_abundantSum = 0
   for i in range(1,ABUNDANT_NO_LIMIT):
-    for number_2 in range(i):
-      if is_abundant():
-        pass
+    if i not in n:
+      non_abundantSum += i
+  
+  return non_abundantSum
 
 
 
+def main():
+  # positive_integers = []
+  abundant_numbers = abundant()       # populates with abundant numbers
+  sum_of_abundant = set()
+  for i in abundant_numbers:
+    for j in abundant_numbers:
+      sum_of_abundant.add(i+j)
+
+  
 
       # if i + number_2 not in abundant_numbers:
       #   positive_integers.append(i)
       #   break
   
   print(f"\nSum of all positive integers Which cannot be \
-     written as sum of two abundant numbers.\n is {sum(positive_integers)}")
+     written as sum of two abundant numbers.\nIs {non_abundant_sum(sum_of_abundant)}")
 
 #Driver Code
 if __name__ == "__main__":
