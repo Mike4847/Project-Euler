fef232331f5b58d897edc6894adacc66bbf02ca8
Mike4847 <michaelelemanl19@students.tukenya.ac.ke>
2025-01-15 23:08:00 +0300

    Writting temporal logic.

---
+++ Reciprocal Cycles/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+/*
+   Problem Statement:
+  In the advent of unit fraction (1/d) there is the realisation of recurrence.
+  in this Problem Statement we ought find the longest recureing sequence,
+  on the bounds of d < 1000.
+
+*/
+
+int main()
+{
+
+  return EXIT_SUCCESS;
+}
