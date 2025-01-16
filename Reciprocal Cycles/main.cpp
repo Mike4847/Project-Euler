9fc1b2a57e60888dc5791da148ccbd47c877256e
Michael Eleman Lopeyok <150112742+Mike4847@users.noreply.github.com>
2025-01-16 22:53:56 +0300

    Update main.cpp

--- Reciprocal Cycles/main.cpp
+++ Reciprocal Cycles/main.cpp
@@ -15,6 +15,7 @@ const int MAXIMUM_DENOMINATOR = 1000;
 
 int main()
 {
+   //NOTE: loop through from 2...to...999
    for(int d = 2; d< MAXIMUM_DENOMINATOR ; d++)
    {
        // creating a set for remainder conditioning.
@@ -23,11 +24,14 @@ int main()
        int remainder_ = NUMERATOR % d;
        
        if (remainders.find(remainder_) != remainders.end())
-       {// Note that there are at most (d-1) unique remainders
-       //this means that in advent a remainder is in a set.
-       // that means that there is a start of recurring.
+       {  // Note that there are at most (d-1) unique remainders
+          //this means that in advent a remainder is in a set.
+          // that means that there is a start of recurring.
         
            
+       }else
+       {
+          
        }
        
    }
