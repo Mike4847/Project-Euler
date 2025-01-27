6d17b1f47ab1107a1c76ecc56b2dd2c5e925804d
Mike4847 <michaelelemanl19@students.tukenya.ac.ke>
2025-01-27 19:33:54 +0300

    Update the solution : Distinct Powers

--- Distinct Powers/main.cpp
+++ Distinct Powers/main.cpp
@@ -1,63 +1,58 @@
 #include <iostream>
-#include <set>
-#include <cmath>
 #include <string>
+#include <vector>
+#include <algorithm>
+#include <set>
+
+using std::string;
+
+//func that returns the power of a number in string format
+string
+multiply(string a, string b)
+{
+
+    if (a=="0" || b=="0")
+    {
+      std::cout<<0<<std::endl;
+      return "0";
+   }
+    int m = a.size() - 1, n = b.size() - 1, carry = 0;
+    string product;
+    for (int i=0; i<=m+n || carry; ++i) {
+        for (int j=std::max(0, i-n); j<=std::min(i, m); ++j)
+            carry += (a[m-j] - '0') * (b[n-i+j] - '0');
+        product += carry % 10 + '0';
+        carry /= 10;
+    }
+    std::reverse(begin(product), end(product));
+    return product;
+}
 
 
+string power(string A, int b)
+{
+    string power= "1";
+    while(b>0)
+    {
+        power = multiply(A,power);
+        b--;
+    }
+    return power;
+}
 
-
-
-
-
-
-std::string pow(int a, int b);
 int main()
 {
-  //  TODO:
-  //note that log(a^b) = b log a
-  std::string product;
-  std::set< std::string > uniqueSequence ;
-
-  //THE idea is a^b in which both the [2<=a=b<=100] 
-  
-
-  
-  for (int a= 2 ; a < 101 ; a++)
-  {
-    for (int b= 2 ; b < 101 ; b++)
+    std::set <string> uniqueProducts; 
+    for (int a = 2 ; a<=100; a++)
     {
-      product = "";
-      
+        for( int b= 2 ; b <=100; b++)
+        {
+            string pow = power(std::to_string(a),b);
+            uniqueProducts.insert(pow);
+            
+        }
     }
+    std::cout << uniqueProducts.size() ;
 
-  }
-  
-  // return the size of the set
-  std::cout << "The size of unique elements of a^ b is\n"
-            << uniqueSequence.size() << std::endl;
-
-
-
+    return 0;
 }
-
-
-
-
-
-std::string 
-pow(std::string a,std::string b)
-{//function that takes in two integers base and powers and returns the equivalence answer but in string type
-  std::string product = "0";
-  for (int i = a.size()-1  ; i >=0 ; i++ )
-  {
-      for (int j = b.size()-1  ; j >=0 ; j++ )
-  {
-      int digitProduct = std::stoi(a[i]) *  std::stoi(b[j]);
-      std::string answer = std::to_string(digitProduct);
-
-    
-  }
-    
-  }
-}
-
