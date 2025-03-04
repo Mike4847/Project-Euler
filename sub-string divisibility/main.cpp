45911c57ba6b7d631dc7629932d414b950b4bc98
Mike4847 <michaelelemanl19@students.tukenya.ac.ke>
2025-03-04 22:36:04 +0300

    Update(Sub-string divisibility) main.cpp

--- sub-string divisibility/main.cpp
+++ sub-string divisibility/main.cpp
@@ -8,6 +8,9 @@
 // todo:
 bool isPanDigital(std::string number);
 bool isSubstrDivisible(std::string number);
+void generatePandigitals(std::string current, std::vector<bool> &used,
+                         std::vector<long long> &results, int position);
+
 int main()
 {
 
@@ -72,4 +75,89 @@ bool isSubstrDivisible(std::string number)
   }
 
   return true;
+}
+
+void generatePandigitals(std::string current, std::vector<bool> &used,
+                         std::vector<long long> &results, int position)
+{
+  // Base case - we've built a 10-digit number
+  if (position == 10)
+  {
+    results.push_back(std::stoll(current));
+    return;
+  }
+
+  // Position 0: can't use 0 as first digit
+  if (position == 0)
+  {
+    for (int i = 1; i <= 9; i++)
+    {
+      if (!used[i])
+      {
+        used[i] = true;
+        generatePandigitals(current + std::to_string(i), used, results, position + 1);
+        used[i] = false;
+      }
+    }
+    return;
+  }
+
+  // For positions 3 and onward, check divisibility constraints
+  if (position >= 3)
+  {
+    int divisor = 0;
+    switch (position)
+    {
+    case 3:
+      divisor = 2;
+      break;
+    case 4:
+      divisor = 3;
+      break;
+    case 5:
+      divisor = 5;
+      break;
+    case 6:
+      divisor = 7;
+      break;
+    case 7:
+      divisor = 11;
+      break;
+    case 8:
+      divisor = 13;
+      break;
+    case 9:
+      divisor = 17;
+      break;
+    }
+
+    if (divisor > 0)
+    {
+      int val = (current[position - 2] - '0') * 100 +
+                (current[position - 1] - '0') * 10;
+
+      // Try only digits that satisfy the divisibility constraint
+      for (int i = 0; i <= 9; i++)
+      {
+        if (!used[i] && (val + i) % divisor == 0)
+        {
+          used[i] = true;
+          generatePandigitals(current + std::to_string(i), used, results, position + 1);
+          used[i] = false;
+        }
+      }
+      return;
+    }
+  }
+
+  // For positions without constraints, try all unused digits
+  for (int i = 0; i <= 9; i++)
+  {
+    if (!used[i])
+    {
+      used[i] = true;
+      generatePandigitals(current + std::to_string(i), used, results, position + 1);
+      used[i] = false;
+    }
+  }
 }
