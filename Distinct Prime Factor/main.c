e31ce49e1364994105a62c87e6371492aeecf1aa
Mike4847 <michaelelemanl19@students.tukenya.ac.ke>
2025-03-12 07:42:30 +0300

    UPdate the Distinct Prime Factor problem Statement

---
+++ Distinct Prime Factor/main.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+
+
+//TODO:
+//calculate prime factorization of a number
+// the idea is using Sieve or Eratosthenes
+
+
+//TODO:
+//calculate list of primes upto a number
+
+int *sieve_of_eratosthenes(int number , int *count){
+
+  /*
+   * The idea is calculating prime upto a number
+   *
+   * */
+  bool *is_prime = (bool *)malloc((number + 1) * sizeof(bool));
+
+  int i ;
+
+  for (i = 0 ; i <= number ; i++)
+  {
+    is_prime[i] = true;
+  }
+
+  // 0 and 1 arent primes 
+  is_prime[0] = is_prime[1] = false;
+
+  for(int p =2 ; p * p <= number ; p++)
+  {
+    if(is_prime[p])
+    {
+      // remove all multiples of p
+      for ( i = p * p  ; i <=number ; i += p )
+      {
+        is_prime[i] = false;
+
+      }
+
+    }
+
+  }
+
+  *count = 0 ;
+  for(i =0 ; i <= number ; i++)
+  {
+    if(is_prime[i]) (*count)++;
+
+  }
+
+  // create an array of primes
+  int *primes = (int *)malloc((*count) * sizeof(int));
+  int index = 0 ;
+  for (i = 2 ; i <= number ; i++)
+  {
+    if(is_prime[i])
+    {
+      primes[index++] = i ;
+    }
+  }
+
+  free(is_prime);
+  return primes;
+
+
+}
+
+
+
+int main(){
+
+
+  int count = 0;
+  int number = 1000000;
+  int *primes = sieve_of_eratosthenes( number, &count);
+
+
+  printf("The number of primes upto %d is %d\n", number, count);
+  printf("List of primes upto %i are:\n", number);
+
+  for (int i=0 ; i < count ; i++ )
+  {
+   printf("%d", primes[i]);
+
+    if(i < count - 1)
+    {
+      printf(", ");
+    }
+
+  }
+
+  printf("\n");
+
+  free(primes);
+
+  return(0);
+}
