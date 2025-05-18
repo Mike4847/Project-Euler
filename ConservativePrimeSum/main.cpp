0f3e40c72b777a24cd43b7aed174a3a82e4d3702
Mike4847 <michaelelemanl19@students.tukenya.ac.ke>
2025-05-18 22:14:02 +0300

    Update main.cpp

--- ConservativePrimeSum/main.cpp
+++ ConservativePrimeSum/main.cpp
@@ -1,6 +1,7 @@
 #include <vector>
 #include <string>
 #include <bitset>
+#include <cmath>
 
 
 const int MAX_SIZE = 1000000;
@@ -32,3 +33,32 @@ int main(){
   
 
 }
+
+std::bitset <MAX_SIZE> SieveOfErastosthene(){
+  /*NOTE(mickey): The calculated Primes below is dependant on the value of the MAX_SIZE
+
+   */
+
+  std::bitset<MAX_SIZE> PrimeAnswer;
+  PrimeAnswer.set();
+
+  // setting the resetting the 0 and 1 bits to false
+  PrimeAnswer.set(0);
+  PrimeAnswer.set(1);
+
+  for (int i = 2; i < sqrt(MAX_SIZE); i++){
+    if(PrimeAnswer[i]){
+
+      for (int p = i * i ; i < MAX_SIZE; p+=i ){
+
+	PrimeAnswer.reset(p);
+
+      }
+
+    }
+
+    }
+  
+
+}
+
