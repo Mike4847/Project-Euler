a6df1525aeb52c174e12bae8a282ec62c714e2f9
Mike4847 <michaelelemanl19@students.tukenya.ac.ke>
2025-05-19 13:50:16 +0300

    Update the answer to the problem statement

--- ConservativePrimeSum/main.cpp
+++ ConservativePrimeSum/main.cpp
@@ -1,8 +1,12 @@
 #include <vector>
-#include <string>
 #include <bitset>
-#include <cmath>
+#include <iostream>
+#include <cstddef>
+#include <numeric>
+#include <utility>
 
+
+using std::size_t;
 
 const int MAX_SIZE = 1000000;
 /*
@@ -11,8 +15,45 @@ const int MAX_SIZE = 1000000;
 
  */
 std::bitset <MAX_SIZE> SieveOfErastosthene();
+bool isPrime(const int Prime);
 
+size_t ConsecutivePrimeSum(const std::vector<int>& primes) {
+    size_t maxLength = 0;           // Track the maximum length of consecutive primes
+    size_t maxPrimeSum = 0;         // Track the corresponding prime sum
+    
+    // For each starting position
+    for (size_t start = 0; start < primes.size(); start++) {
+        int sum = 0;
+        // Try consecutive primes starting from this position
+        for (size_t length = 0; start + length < primes.size(); length++) {
+            sum += primes[start + length];
+            
+            // Break if sum exceeds our limit
+            if (sum >= MAX_SIZE) break;
+            
+            // If sum is prime and sequence is longer than our current max
+            if (isPrime(sum) && (length + 1) > maxLength) {
+                maxLength = length + 1;
+                maxPrimeSum = sum;
+            }
+        }
+    }
+    return maxPrimeSum;
+}
+//TODO(mickey):
+//creating a utility function for checking if a number is prime or not
 
+bool isPrime(const int Prime) {
+    if (Prime < 2) return false;
+    if (Prime == 2) return true;
+    if (Prime % 2 == 0) return false;  // Check even numbers first
+    for (int i = 3; i * i <= Prime; i += 2) {  // Only check odd numbers
+        if (Prime % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
 int main(){
 
   
@@ -21,7 +62,7 @@ int main(){
 
   std::bitset<MAX_SIZE> PrimeBits = SieveOfErastosthene();
 
-  for(int i=0; i <= MAX_SIZE ; i++ ){
+  for(int i=0; i < MAX_SIZE ; i++ ){
 
     if (PrimeBits[i]){
       Prime.push_back(i);
@@ -29,8 +70,10 @@ int main(){
     }
   }
 
-  
-  
+
+  size_t ConsecutiveSumPrime =  ConsecutivePrimeSum(Prime);
+
+  std::cout << "The largest Consecutive Sum of Prime of numbers below 1,000,000 is " << ConsecutiveSumPrime <<std::endl;
 
 }
 
@@ -43,13 +86,13 @@ std::bitset <MAX_SIZE> SieveOfErastosthene(){
   PrimeAnswer.set();
 
   // setting the resetting the 0 and 1 bits to false
-  PrimeAnswer.set(0);
-  PrimeAnswer.set(1);
+  PrimeAnswer.reset(0);
+  PrimeAnswer.reset(1);
 
-  for (int i = 2; i < sqrt(MAX_SIZE); i++){
+  for (int i = 2; i * i < MAX_SIZE; i++){
     if(PrimeAnswer[i]){
 
-      for (int p = i * i ; i < MAX_SIZE; p+=i ){
+      for (int p = i * i ; p < MAX_SIZE; p+=i ){
 
 	PrimeAnswer.reset(p);
 
@@ -57,7 +100,9 @@ std::bitset <MAX_SIZE> SieveOfErastosthene(){
 
     }
 
-    }
+   }
+
+  return PrimeAnswer;
   
 
 }
